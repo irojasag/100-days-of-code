376a7143d2c8dc96b882e88b716a56a55975d304
irojasag <imoon521@gmail.com>
2025-12-01 13:36:42 +0100

    001 python

---
+++ python/001/001.py
@@ -0,0 +1,5 @@
+print("Let's create a rock band name!")
+color = input("Enter your favorite color: ")
+pet = input("Enter your pet's name: ")
+
+print(f"Your rock band name could be: {color} {pet}!")
