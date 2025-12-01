cf2e8e5413bcabd1a6522b5cff78042569215982
irojasag <imoon521@gmail.com>
2025-12-01 13:34:07 +0100

    001 bash

---
+++ bash/001/001.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "This is a bash script."
+echo "Lets create a rock band name!"
+
+read -p "Enter your favorite color: " color
+read -p "Enter your pet's name: " pet
+
+echo "Your rock band name could be: ${color} ${pet}!"
